4ade7368a678ab2d304fcbf8261fdbbe58ff329b
Lívia F Lo Re <106773988+liviaflore@users.noreply.github.com>
2022-08-08 23:33:06 -0300

    CRUD Postagem

---
+++ Blog Pessoal/CRUD/BlogPessoal/src/main/java/br/com/generation/controller/PostagemController.java
@@ -0,0 +1,28 @@
+package br.com.generation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import br.com.generation.repository.PostagemRepository;
+import br.com.generation.model.Postagem;
+import java.util.List;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository postagemRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> getAll(){
+		return ResponseEntity.ok(postagemRepository.findAll());
+	}
+
+}
---
+++ Blog Pessoal/CRUD/BlogPessoal/src/main/java/br/com/generation/model/Postagem.java
@@ -0,0 +1,66 @@
+package br.com.generation.model;
+
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		
+		@NotBlank(message = "O atributo título é obrigatório!")
+		@Size(min = 3 , max = 255, message = "O atributo título deve conter no mínimo 3 e no máximo 255 caracteres")
+		private String titulo;
+		
+		@NotBlank(message = "O atributo texto é obrigatório!")
+		@Size(min = 3 , max = 1000, message = "O atributo título deve conter no mínimo 3 e no máximo 1000 caracteres")
+		private String texto;
+		
+		@UpdateTimestamp
+		private LocalDateTime data;
+		
+		
+		
+		
+		public Long getId() {
+			return this.id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getTitulo() {
+			return this.titulo;
+		}
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+		public String getTexto() {
+			return this.texto;
+		}
+		public void setTexto(String texto) {
+			this.texto = texto;
+		}
+		public LocalDateTime getData() {
+			return this.data;
+		}
+		public void setData(LocalDateTime data) {
+			this.data = data;
+		}
+		
+		
+
+
+}
+
